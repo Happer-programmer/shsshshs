272d14cee9c9059b9457d7576e603655b1f5ffa5
Mr Saif <85456658+Happer-programmer@users.noreply.github.com>
2021-10-25 18:51:56 +0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:Happy_Birthday/startingscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: startingScreen());
+  }
+}
